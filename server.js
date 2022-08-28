5ebd2c06a4323e0e7069849855fb9c11c338c3a3
Shubhankar395 <90629016+Shubhankar395@users.noreply.github.com>
2022-08-28 16:43:58 +0530

    for render ui

--- server.js
+++ server.js
@@ -34,6 +34,13 @@ app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  });
+ }
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 
