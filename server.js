b7b1b854662135dcf93a563de89fa5a8245b1d8f
Shubhankar395 <90629016+Shubhankar395@users.noreply.github.com>
2022-08-28 18:02:31 +0530

    for render

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const port = process.env.PORT || 5000;
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    res.sendFile(path.join('/client/build/index.html'));
   });
  }
 
